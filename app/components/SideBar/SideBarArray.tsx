1de636a80d4d52fb6a51d349bfb2c7d3cf277b20
repos-evoly <repos@evo-ly.com>
2024-12-04 12:47:54 +0200

    SideBar

--- app/components/SideBar/SideBarArray.tsx
+++ app/components/SideBar/SideBarArray.tsx
@@ -16,93 +16,125 @@ import { BsJournalArrowUp } from "react-icons/bs";
 // Define your items without translation logic
 export const sideBarItemsArray = [
   {
-    href: "/main/Contact",
-    labelKey: "contact",
+    href: "#",
+    labelKey: "statementOfAccount",
     icon: <FaUserPlus />,
     tip: "Contact",
   },
   {
-    href: "/main/Contract",
-    labelKey: "contract",
-    icon: <FaFileContract />,
-    tip: "Contract",
-  },
-  {
-    href: "/main/Accounts",
-    labelKey: "accounts",
-    icon: <FaUsers />,
-    tip: "Accounts",
-  },
-  {
     href: "#",
-    labelKey: "invoice.label",
+    labelKey: "transfer.label",
     icon: <FaFileInvoice />, // Updated for Financial Statements
-    tip: "invoice.label",
+    tip: "transfer.label",
     combo: true,
     subItems: [
       {
-        href: "/main/Invoice",
-        labelKey: "invoice.purchase",
+        href: "#",
+        labelKey: "transfer.inBank",
         icon: <FaFileInvoice />, // Updated for Statement of Accounts
         tip: "Purchase Invoice",
       },
       {
-        href: "/main/salesInvoice",
-        labelKey: "invoice.sales",
+        href: "#",
+        labelKey: "transfer.betweenBanks",
         icon: <FaFileInvoice />, // Updated for Trial Balance
-        tip: "SalesInvoice",
+        tip: "Transfer between banks",
+      },
+      {
+        href: "#",
+        labelKey: "transfer.out",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "External transfers",
       },
     ],
   },
+
   {
-    href: "/main/Chart",
-    labelKey: "chart",
-    icon: <MdOutlineSwitchAccount />,
-    tip: "Chart Of Accounts",
-  },
-  {
-    href: "/main/Currencies",
-    labelKey: "currencies",
-    icon: <GrCurrency />,
-    tip: "Currencies",
-  },
-  {
-    href: "/main/journal",
-    labelKey: "journal",
-    icon: <BsJournalArrowUp />,
-    tip: "Journal",
-  },
-  {
-    href: "/main/Products",
-    labelKey: "products",
-    icon: <FaBox />,
-    tip: "Products",
-  },
-  {
-    href: "/main/financialStatements",
-    labelKey: "financial.label",
-    icon: <FaRegFileAlt />, // Updated for Financial Statements
-    tip: "Financial Statements",
+    href: "#",
+    labelKey: "requests.label",
+    icon: <FaFileInvoice />, // Updated for Financial Statements
+    tip: "requests.label",
     combo: true,
     subItems: [
       {
-        href: "/main/statement-of-accounts",
-        labelKey: "financial.accounts",
-        icon: <FaRegFileAlt />, // Updated for Statement of Accounts
-        tip: "Statement of Accounts",
+        href: "#",
+        labelKey: "requests.checkBook",
+        icon: <FaFileInvoice />, // Updated for Statement of Accounts
+        tip: "requests.checkBook",
       },
       {
-        href: "/main/trialBalance",
-        labelKey: "financial.trialBalance",
-        icon: <FaBalanceScale />, // Updated for Trial Balance
-        tip: "Trial Balance",
+        href: "#",
+        labelKey: "requests.certifiedCheck",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.certifiedCheck",
       },
       {
-        href: "/main/financialStatements/balance-sheet",
-        labelKey: "financial.balanceSheet",
-        icon: <FaBook />, // Updated for Balance Sheet
-        tip: "Balance Sheet",
+        href: "#",
+        labelKey: "requests.guaranteeLetter",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.guaranteeLetter",
+      },
+      {
+        href: "#",
+        labelKey: "requests.creditFacility",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.creditFacility",
+      },
+      {
+        href: "#",
+        labelKey: "requests.visa",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.visa",
+      },
+      {
+        href: "#",
+        labelKey: "requests.rtes",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.rtes",
+      },
+      {
+        href: "#",
+        labelKey: "requests.foreign",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.foreign",
+      },
+      {
+        href: "#",
+        labelKey: "requests.cbl",
+        icon: <FaFileInvoice />, // Updated for Trial Balance
+        tip: "requests.cbl",
       },
     ],
+  },
+
+  {
+    href: "#",
+    labelKey: "localization",
+    icon: <FaUserPlus />,
+    tip: "localization",
+  },
+  {
+    href: "#",
+    labelKey: "documentary",
+    icon: <FaUserPlus />,
+    tip: "documentary",
+  },
+  {
+    href: "#",
+    labelKey: "followUp",
+    icon: <FaUserPlus />,
+    tip: "Follow Up",
+  },
+  {
+    href: "#",
+    labelKey: "settings",
+    icon: <FaUserPlus />,
+    tip: "settings",
+  },
+  {
+    href: "#",
+    labelKey: "currRate",
+    icon: <FaUserPlus />,
+    tip: "currRate",
   },
 ];
--- app/components/mainHeader/MainHeader.tsx
+++ app/components/mainHeader/MainHeader.tsx
@@ -1,4 +1,3 @@
-// src/components/MainHeader.tsx
 import React from "react";
 import Image from "next/image";
 import { MainHeaderProps } from "@/types"; // Assuming you have your types defined in "@/types"
@@ -6,21 +5,19 @@ import { useTranslations } from "next-intl";
 
 const MainHeader: React.FC<MainHeaderProps> = ({ logoUrl, isRtl }) => {
   const t = useTranslations("mainHeader");
+
   return (
     <header className="w-full h-20 flex items-center px-5 bg-info-dark">
       <div
-        style={{
-          order: isRtl ? 2 : 0, // Position logo on the right if RTL, otherwise on the left
-          marginLeft: isRtl ? "auto" : 0,
-          marginRight: isRtl ? 0 : "auto",
-        }}
+        className={`flex-shrink-0 ${
+          isRtl ? "order-2 ml-auto" : "order-0 mr-auto"
+        } ${isRtl ? "scale-x-[-1]" : ""}`}
       >
         <Image
           src={logoUrl}
           alt="Logo"
           width={48} // Adjust width as necessary
           height={48} // Adjust height as necessary
-          layout="fixed" // Using fixed layout for defined width and height
         />
       </div>
       <h1 className="flex-1 text-center text-2xl font-semibold">

1a90e59797c6f2127b7c17707f411ff0f2d22244
repos-evoly <repos@evo-ly.com>
2024-12-08 16:33:13 +0200

    RTGSForm

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,12 @@
-import CBLForm from "../components/forms/CBLForm/CBLForm";
+// import CBLForm from "../components/forms/CBLForm/CBLForm";
+// import RTGSForm from "../components/forms/RTGSForm/RTGSForm";
 
 export default function HomePage() {
   return (
     <div>
-      <CBLForm />
+      {/* <CBLForm /> */}
+      {/* <RTGSForm /> */}
+      hello
     </div>
   );
 }
---
+++ app/components/reusable/CheckBox.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface CheckboxProps {
+  label: string; // The title to display beside the checkbox
+  checked?: boolean; // Optional to control the checked state
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // Event handler for the checkbox
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  checked = false,
+  onChange,
+}) => {
+  return (
+    <label className="flex items-center text-sm font-medium text-gray-700">
+      <input
+        type="checkbox"
+        className="rtl:ml-2 ltr:mr-2"
+        checked={checked}
+        onChange={onChange}
+      />
+      {label}
+    </label>
+  );
+};
+
+export default Checkbox;

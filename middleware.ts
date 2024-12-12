0e4fd3f572e6259880b3e27c3734ab6eb2ad4472
repos-evoly <repos@evo-ly.com>
2024-12-12 15:51:28 +0200

    fixing locale

--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,22 @@ import createMiddleware from 'next-intl/middleware';
 import { NextRequest, NextResponse } from 'next/server';
 
 const intlMiddleware = createMiddleware({
-  locales: ['en', 'ar'], // Supported locales
-  defaultLocale: 'ar',   // Default locale
+  locales: ['en', 'ar'],
+  defaultLocale: 'ar',
 });
 
 export default function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
-  const pathname = url.pathname;
 
-  // Check if the route already includes a locale
-  const localeMatch = /^\/(ar|en)(\/|$)/.test(pathname);
-
-  // If no locale is found in the path, redirect to defaultLocale ('ar')
-  if (!localeMatch) {
-    url.pathname = `/ar${pathname}`;
+  // Force default locale for the root path (`/`)
+  if (url.pathname === '/') {
+    url.pathname = `/ar`;
     return NextResponse.redirect(url);
   }
 
-  // Use intlMiddleware for locale-based routing
   return intlMiddleware(request);
 }
 
 export const config = {
-  matcher: [
-    '/:path*', // Apply to all routes
-  ],
+  matcher: ['/', '/(ar|en)/:path*'],
 };
